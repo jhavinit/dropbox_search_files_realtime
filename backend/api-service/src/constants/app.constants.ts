3f6804ce2c1bc729595237ae9e9ccd5da76c0750
vinitjha <vinit@nubewell.com>
2024-11-30 19:39:38 +0530

    Updated env code to handle render deployment and added support for remote elastic connect

--- backend/api-service/src/constants/app.constants.ts
+++ backend/api-service/src/constants/app.constants.ts
@@ -1,6 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
-export const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL || 'http://localhost:9200';
+export const PORT = process.env.PORT as string;
+export const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL as string;
+export const ELASTIC_USERNAME = process.env.ELASTIC_USERNAME as string;
+export const ELASTIC_PASSWORD = process.env.ELASTIC_PASSWORD as string;
 export const FILE_INDEX_NAME = 'files';
--- backend/dropbox-service/src/services/elastic.service.ts
+++ backend/dropbox-service/src/services/elastic.service.ts
@@ -2,7 +2,7 @@ import { Client } from "@elastic/elasticsearch";
 import { handleErrors } from "../utils/error.decorator";
 import { logger } from "../utils/logger";
 import { IFileMetadata } from "../interfaces/file.interface";
-import { ELASTICSEARCH_URL, FILE_INDEX_NAME } from "../constants/app.constants";
+import { ELASTIC_PASSWORD, ELASTIC_USERNAME, ELASTICSEARCH_URL, FILE_INDEX_NAME } from "../constants/app.constants";
 
 export class ElasticService {
     private client: Client;
@@ -10,6 +10,10 @@ export class ElasticService {
     constructor() {
         this.client = new Client({
             node: ELASTICSEARCH_URL,
+            auth: {
+                username: ELASTIC_USERNAME,      // Replace with your username (if using basic auth)
+                password: ELASTIC_PASSWORD       // Replace with your password (if using basic auth)
+          },            
         });
     }
 
@@ -33,67 +37,137 @@ export class ElasticService {
     @handleErrors()
     async createIndex(): Promise<void> {
         const indexExists = await this.client.indices.exists({
-            index: FILE_INDEX_NAME,
+            index: FILE_INDEX_NAME
         });
-        if (!indexExists.body) {
+
+        if (!indexExists) {
             await this.client.indices.create({
                 index: FILE_INDEX_NAME,
-                body: {
-                    "settings": {
-                        "index.max_ngram_diff": 7,
-                      "analysis": {
+                settings: {
+                    "index.max_ngram_diff": 7,
+                    "analysis": {
                         "analyzer": {
-                          "custom_analyzer": {
-                            "tokenizer": "standard",
-                            "filter": [
-                              "lowercase",
-                              "synonym_filter",
-                              "ngram_filter"
-                            ]
-                          },
-                          "search_analyzer": {
-                            "tokenizer": "standard",
-                            "filter": [
-                              "lowercase",
-                              "synonym_filter"
-                            ]
-                          }
+                            "custom_analyzer": {
+                                "type": "custom",
+                                "tokenizer": "standard",
+                                "filter": [
+                                    "lowercase",
+                                    "synonym_filter",
+                                    "ngram_filter"
+                                ]
+                            },
+                            "search_analyzer": {
+                                "type": "custom",
+                                "tokenizer": "standard",
+                                "filter": [
+                                    "lowercase",
+                                    "synonym_filter"
+                                ]
+                            }
                         },
                         "filter": {
-                          "synonym_filter": {
-                            "type": "synonym",
-                            "synonyms": [
-                              "hi, hello"  // Add more synonyms as needed
-                            ]
-                          },
-                          "ngram_filter": {
-                            "type": "ngram",
-                            "min_gram": 3,
-                            "max_gram": 10
-                          }
+                            "synonym_filter": {
+                                "type": "synonym",
+                                "synonyms": [
+                                    "hi, hello"  // Add more synonyms as needed
+                                ]
+                            },
+                            "ngram_filter": {
+                                "type": "ngram",
+                                "min_gram": 3,
+                                "max_gram": 10
+                            }
                         }
-                      }
-                    },
-                    "mappings": {
-                      "properties": {
-                        "text": {
-                          "type": "text",
-                          "analyzer": "custom_analyzer",
-                          "search_analyzer": "search_analyzer"
-                        },
-                        "filename": {
-                          "type": "keyword"
-                        },
-                        "url": {
-                          "type": "keyword"
-                        }
-                      }
                     }
-                  }
+                },
+                mappings: {
+                    properties: {
+                        text: {
+                            type: "text",
+                            analyzer: "custom_analyzer",
+                            search_analyzer: "search_analyzer"
+                        },
+                        filename: {
+                            type: "keyword"
+                        },
+                        url: {
+                            type: "keyword"
+                        }
+                    }
+                }
             });
             logger.info(`Created index: ${FILE_INDEX_NAME}`);
         } else {
             logger.info(`Index ${FILE_INDEX_NAME} already exists`);
         }
     }    
+
+
+
+    // @handleErrors()
+    // async createIndex(): Promise<void> {
+    //     const indexExists = await this.client.indices.exists({
+    //         index: FILE_INDEX_NAME,
+    //     });
+    //     if (!indexExists.body) {
+    //         await this.client.indices.create({
+    //             index: FILE_INDEX_NAME,
+    //             body: {
+    //                 "settings": {
+    //                     "index.max_ngram_diff": 7,
+    //                   "analysis": {
+    //                     "analyzer": {
+    //                       "custom_analyzer": {
+    //                         "tokenizer": "standard",
+    //                         "filter": [
+    //                           "lowercase",
+    //                           "synonym_filter",
+    //                           "ngram_filter"
+    //                         ]
+    //                       },
+    //                       "search_analyzer": {
+    //                         "tokenizer": "standard",
+    //                         "filter": [
+    //                           "lowercase",
+    //                           "synonym_filter"
+    //                         ]
+    //                       }
+    //                     },
+    //                     "filter": {
+    //                       "synonym_filter": {
+    //                         "type": "synonym",
+    //                         "synonyms": [
+    //                           "hi, hello"  // Add more synonyms as needed
+    //                         ]
+    //                       },
+    //                       "ngram_filter": {
+    //                         "type": "ngram",
+    //                         "min_gram": 3,
+    //                         "max_gram": 10
+    //                       }
+    //                     }
+    //                   }
+    //                 },
+    //                 "mappings": {
+    //                   "properties": {
+    //                     "text": {
+    //                       "type": "text",
+    //                       "analyzer": "custom_analyzer",
+    //                       "search_analyzer": "search_analyzer"
+    //                     },
+    //                     "filename": {
+    //                       "type": "keyword"
+    //                     },
+    //                     "url": {
+    //                       "type": "keyword"
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //         });
+    //         logger.info(`Created index: ${FILE_INDEX_NAME}`);
+    //     } else {
+    //         logger.info(`Index ${FILE_INDEX_NAME} already exists`);
+    //     }
+    // }    
 }
